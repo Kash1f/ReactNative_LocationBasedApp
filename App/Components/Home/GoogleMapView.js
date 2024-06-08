d4453c43a90e615112ec043ba963e8940b4e4253
Kashif <ahmed.kashif1001@gmail.com>
2024-06-08 13:09:30 +0500

    changes

--- App/Components/Home/GoogleMapView.js
+++ App/Components/Home/GoogleMapView.js
@@ -1,17 +1,16 @@
 import { View, Text, Dimensions } from 'react-native'
 import React from 'react'
-import MapView from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 export default function GoogleMapView() {
   return (
-    <View>
+    <View style={{marginTop:20}}>
       <MapView 
       style={{
         width: Dimensions.get('screen').width*0.9,
-        height:Dimensions.get('screen').height*0.23
-        
+        height:Dimensions.get('screen').height*0.23,
       }}
-      
+      provider={PROVIDER_GOOGLE}
       ></MapView>
     </View>
   )
